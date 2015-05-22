c000b92aea4d078511ed00c4ac13677f4a4aec29
Rich <rich.edwards@cheekybirdie.com>
2015-05-21 23:07:08 -0700

    Initial Algorithm and Ember training
    Initial Algorithm and Ember training

---
+++ AlgorithmsAndDataStructuresPart1/LinkedList/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList {
+    class Program {
+        static void Main(string[] args) {
+            LinkedListDemo();
+        }
+
+        private static void LinkedListDemo() {
+            var numbers = new LinkedList<int>();
+            numbers.AddHead(8);
+            numbers.AddHead(4);
+            numbers.AddHead(2);
+            numbers.AddHead(1);
+            Console.WriteLine(numbers.ToString());
+
+            foreach (var nbr in numbers) {
+                Console.WriteLine("number: " + nbr.value);
+            }
+
+            var words = new LinkedList<string>();
+            words.AddTail("four");
+            words.AddTail("score");
+            words.AddTail("and");
+            words.AddTail("seven");
+            words.AddTail("years");
+            words.AddTail("ago");
+            Console.WriteLine(words.ToString());
+            words.RemoveHead();
+            Console.WriteLine(words.ToString());
+            words.RemoveHead();
+            Console.WriteLine(words.ToString());
+            words.RemoveTail();
+            Console.WriteLine(words.ToString());
+            words.RemoveTail();
+            Console.WriteLine(words.ToString());
+            words.RemoveTail();
+            Console.WriteLine(words.ToString());
+            words.RemoveTail();
+            Console.WriteLine(words.ToString());
+            words.RemoveTail();
+            Console.WriteLine(words.ToString());
+            words.RemoveTail();
+            words.RemoveTail();
+            words.RemoveHead();
+            Console.WriteLine(words.ToString());
+        }
+
+        static void ChainDemo() {
+            Node<int> head = new Node<int>(3);
+
+            head.Next = new Node<int>(5);
+            head.Next.Next = new Node<int>(7);
+
+            DisplayChain(head);
+
+            Console.ReadKey();
+        }
+
+        static void DisplayChain(Node<int> node) {
+            while (node != null) {
+                Console.WriteLine("value: " + node.Value);
+                node.Value *= 2;
+                node = node.Next;
+            }
+
+            var foo = new Node<int>(2839);
+            node = foo;
+        }
+    }
+}
---
+++ EmberJSFundamentals/scripts/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Cindy Edwards on 5/21/2015.
+ */
+window.Github = Ember.Application.create({});
