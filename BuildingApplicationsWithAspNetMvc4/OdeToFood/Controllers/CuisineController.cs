4790f6f2b751252b09713c35fcd03ea521b05535
Rich <rich.edwards@cheekybirdie.com>
2015-05-27 19:06:52 -0700

    module 2 updates

---
+++ BuildingApplicationsWithAspNetMvc4/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,64 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+        //[AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
+        //[HttpGet]
+        public ActionResult Search(string name = "guyanese")
+        {
+            //var name = RouteData.Values["name"];
+
+            // deter against malicious input
+            var msg = Server.HtmlEncode("GET: Here is some info on " + name + " cuisine");
+
+            return Content(msg);
+
+            //return RedirectToAction("Index", "Home", new {name = name});
+
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About", foo = "bar", name = name, id = name });
+
+            var bytes = Encoding.UTF8.GetBytes(msg);
+            var stream = new MemoryStream(bytes);
+            //return File(stream, "text/plain");
+
+            //return File(Server.MapPath("~/Content/Site.css"), "text/css");
+
+            //return Json(new AboutModel() { Name = name, Location = "Earth" }, JsonRequestBehavior.AllowGet);
+        }
+
+        /*[HttpPost]
+        public ActionResult Search() {
+            var name = RouteData.Values["name"];
+
+            // deter against malicious input
+            var msg = Server.HtmlEncode("POST: Here is some info on " + name + " cuisine");
+
+            return Content(msg);
+
+            //return RedirectToAction("Index", "Home", new {name = name});
+
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About", foo = "bar", name = name, id = name });
+
+            var bytes = Encoding.UTF8.GetBytes(msg);
+            var stream = new MemoryStream(bytes);
+            //return File(stream, "text/plain");
+
+            //return File(Server.MapPath("~/Content/Site.css"), "text/css");
+
+            //return Json(new AboutModel() { Name = name, Location = "Earth" }, JsonRequestBehavior.AllowGet);
+        }*/
+
+    }
+}
---
+++ BuildingApplicationsWithAspNetMvc4/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            ViewBag.Message = string.Format("{0}::{1}  {2}", controller, action, id);
+
+            return View();
+        }
+
+        public ActionResult About() {
+            var model = new AboutModel() { Name = "Rich E", Location = "Bellevue" };
+
+            return View(model);
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
