c2b683c173980dda06c3ebb1ee0c291e992cc4ed
Rich <rich.edwards@cheekybirdie.com>
2015-05-28 12:30:39 -0700

    TPL Async files

---
+++ TplAsync/SlowWeb/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace SlowWeb.Controllers {
+    public class ValuesController : ApiController {
+        // GET api/values
+        public IEnumerable<string> Get() {
+
+            // slow down
+            System.Threading.Thread.Sleep(10000);
+
+            var path = HttpContext.Current.Server.MapPath("~/App_Data/sowpods_europe.txt");
+
+            var content = "";
+
+            if (File.Exists(path)) {
+                content = File.ReadAllText(path);
+            }
+
+            //return new string[] { "value1", "value2" };
+            return new string[] {content};
+        }
+
+        // GET api/values/5
+        public string Get(int id) {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value) {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value) {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ TplAsync/UsingTaskBasedApi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingTaskBasedApi {
+    class Program {
+        static void Main(string[] args) {
+            DemoThreadBasedTask();
+        }
+
+        public static void DemoThreadBasedTask() {
+            var webClient = new WebClient();
+
+            var url = "http://localhost:13511/api/values";
+
+            // the thread is blocked until the DownloadString call completes
+            var result = webClient.DownloadString(url);
+            Console.WriteLine(result.Length + " bytes returned");
+        }
+    }
+}
